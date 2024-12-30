83ac0eee037b068ed66b0718ee91d4ff9b28ffb7
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2024-12-30 21:16:40 +0100

    feat: add autoscaler to mig

--- modules/mig/mig.tf
+++ modules/mig/mig.tf
@@ -34,4 +34,22 @@ resource "google_compute_region_instance_group_manager" "mig" {
     health_check      = google_compute_health_check.mig-health-check.id
     initial_delay_sec = 300
   }
+  wait_for_instances = true
+
+}
+
+
+resource "google_compute_region_autoscaler" "mig-autoscaler" {
+  name   = "mig-autoscaler"
+  target = google_compute_region_instance_group_manager.mig.id
+
+  autoscaling_policy {
+    max_replicas    = 5
+    min_replicas    = 2
+    cooldown_period = 60
+
+    cpu_utilization {
+      target = 0.5
+    }
+  }
 }
