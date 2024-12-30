1ffac5a60dc63ecbad2a8e6a6f62aef44cfd5e9e
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2024-12-30 22:17:33 +0100

    feat: add redis cluster

--- modules/memory_store/main.tf
+++ modules/memory_store/main.tf
@@ -1,38 +1,38 @@
-# resource "google_redis_cluster" "redis-cluster" {
-#   name        = "redis-cluster"
-#   shard_count = 3
-#   psc_configs {
-#     network = data.google_compute_network.prod-network.id
-#   }
-#   region                  = var.region
-#   replica_count           = 1
-#   node_type               = "REDIS_SHARED_CORE_NANO"
-#   transit_encryption_mode = "TRANSIT_ENCRYPTION_MODE_DISABLED"
-#   authorization_mode      = "AUTH_MODE_DISABLED"
-#   redis_configs = {
-#     maxmemory-policy = "volatile-ttl"
-#   }
-#   deletion_protection_enabled = false
-#
-#   zone_distribution_config {
-#     mode = "MULTI_ZONE"
-#   }
-#   maintenance_policy {
-#     weekly_maintenance_window {
-#       day = "MONDAY"
-#       start_time {
-#         hours   = 1
-#         minutes = 0
-#         seconds = 0
-#         nanos   = 0
-#       }
-#     }
-#   }
-#   depends_on = [
-#     google_network_connectivity_service_connection_policy.redis-connectivity-policy
-#   ]
-#
-# }
+resource "google_redis_cluster" "redis-cluster" {
+  name        = "redis-cluster"
+  shard_count = 3
+  psc_configs {
+    network = data.google_compute_network.prod-network.id
+  }
+  region                  = var.region
+  replica_count           = 1
+  node_type               = "REDIS_SHARED_CORE_NANO"
+  transit_encryption_mode = "TRANSIT_ENCRYPTION_MODE_DISABLED"
+  authorization_mode      = "AUTH_MODE_DISABLED"
+  redis_configs = {
+    maxmemory-policy = "volatile-ttl"
+  }
+  deletion_protection_enabled = false
+
+  zone_distribution_config {
+    mode = "MULTI_ZONE"
+  }
+  maintenance_policy {
+    weekly_maintenance_window {
+      day = "MONDAY"
+      start_time {
+        hours   = 1
+        minutes = 0
+        seconds = 0
+        nanos   = 0
+      }
+    }
+  }
+  # depends_on = [
+  #   google_network_connectivity_service_connection_policy.redis-connectivity-policy
+  # ]
+
+}
 
 # resource "google_network_connectivity_service_connection_policy" "redis-connectivity-policy" {
 #   name          = "redis-connectivity-policy"
