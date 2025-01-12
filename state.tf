c9f3fb509669cb13d435c67f96e08dde471729a5
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2025-01-12 14:56:27 +0100

    fix: ci

--- state.tf
+++ state.tf
@@ -11,22 +11,6 @@ resource "google_storage_bucket" "default" {
   }
 }
 
-resource "local_file" "default" {
-  file_permission = "0644"
-  filename        = "${path.module}/backend.tf"
-
-  # You can store the template in a file and use the templatefile function for
-  # more modularity, if you prefer, instead of storing the template inline as
-  # we do here.
-  content = <<-EOT
-  terraform {
-    backend "gcs" {
-      bucket = "${google_storage_bucket.default.name}"
-    }
-  }
-  EOT
-}
-
 resource "google_service_account" "github-actions-ci-cd" {
   account_id   = "github-actions-ci-cd"
   display_name = "GitHub Actions CI/CD"
