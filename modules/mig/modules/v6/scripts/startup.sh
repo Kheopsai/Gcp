cb62d0d8beb78e4b0a6c70dd924159b2f53825ac
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2025-03-17 19:57:35 +0100

    fix: permissions

--- modules/mig/modules/v6/scripts/startup.sh
+++ modules/mig/modules/v6/scripts/startup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 sudo su
 mkdir -p /root/.ssh && touch /root/.ssh/authorized_keys && echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDmiFxUhuikeF/o957k6Z4yua8sf4vmZtofrt2vGikg3ob86Tt+cQcsej0PHLMKzYXNyb+2v41UYEzHPXHwucOx8ywxSN9Lfzya7fz2LYc61FcSrjxfURWHg3BlZp+dK6wchg7YqvM1pFkqkBV99Y0z5RAul5fngip9sT6fH3RDm8enetMjAC8JN7kamqiMwUE2C0FUblaEIsfFnGKiEv3YmRdBuSxPFc2upTAd1D14cCFPoCT2d1G7CwmI2LhDYs+ESpCnn+gI2VVvQRvA6UaHAwi4aRu+sQUHmfTCsK0nqe5G77Bqu4Nc0QXCLMmX7du2gS/Vy6MjzHauZ8mtHUin' >> /root/.ssh/authorized_keys
+
+# Set Permissions
+chmod 700 /root/.ssh && chmod 600 /root/.ssh/authorized_keys
+
 # Récupérer les métadonnées de la VM GCP
 IP=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip" -H "Metadata-Flavor: Google")
 PORT=22
