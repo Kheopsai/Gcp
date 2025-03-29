e78954bd54cc649e1ec1e6047a75da88ed7d4101
AkramZerarka <akram.zerarka@gmail.com>
2025-03-29 17:41:24 +0100

    Create v2

--- modules/mig/templates.tf
+++ modules/mig/templates.tf
@@ -7,3 +7,12 @@ module "v1" {
   service_account_email = google_service_account.mig-sa.email
   kheops_auth_token     = var.kheops_auth_token
 }
+module "v2" {
+  source                = "./modules/v2"
+  project_id            = var.project_id
+  network               = var.network
+  region                = var.region
+  subnet                = var.subnet
+  service_account_email = google_service_account.mig-sa.email
+  kheops_auth_token     = var.kheops_auth_token
+}
