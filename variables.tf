7e4c49b1ce5583e87d7c094a7a2485b7fd808e16
AkramZerarka <akram.zerarka@gmail.com>
2025-06-21 13:16:39 +0200

    Improve script

--- variables.tf
+++ variables.tf
@@ -1,3 +1,12 @@
+variable "project_id" {
+  description = "The project ID to deploy resources"
+  type        = string
+}
+variable "region" {
+  description = "The region to deploy resources"
+  type        = string
+}
+
 variable "network" {
   description = "The network to deploy the MIG"
   type        = string
