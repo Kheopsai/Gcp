6f97d0895954c532b68a60843c6c098d32dd8cce
AkramZerarka <akram.zerarka@gmail.com>
2025-06-21 13:11:05 +0200

    Improve script

--- modules/mig/templates.tf
+++ modules/mig/templates.tf
@@ -6,6 +6,7 @@ module "v1" {
   subnet                = var.subnet
   service_account_email = google_service_account.mig-sa.email
   kheops_auth_token     = var.kheops_auth_token
+  kheops_project_name   = var.kheops_project_name
 }
 module "v2" {
   source                = "./modules/v2"
@@ -15,4 +16,5 @@ module "v2" {
   subnet                = var.subnet
   service_account_email = google_service_account.mig-sa.email
   kheops_auth_token     = var.kheops_auth_token
+  kheops_project_name     = var.kheops_project_name
 }
